d28786bfeadcaf2a9559c30071a71d528a4c6729
omeaku chinyere <omeakuchinyere0713@gmail.com>
2023-04-27 05:28:05 -0700

    my commit

--- interpreter.c
+++ interpreter.c
@@ -1,39 +1,40 @@
 #include "main.h"
+
 /**
- * analyze - analyze argument
+ * analyse - analyse argument
  * @arguments: arguments and commands to execute.
  * @info: general informations about the shell.
  * @buff: lines re-added
  */
 
-void analyze(char **arguments, char *buff, general_t *info)
+void analyse(char **arguments, char *buff, general_t *info)
 {
 	int status;
 	char *comd;
 
-if (*arguments == NULL || arguments == NULL)
+	if (*arguments == NULL || arguments == NULL)
 	return (0);
 
-comd = arguments[0];
-info->command = comd;
-if (check_builtin(info, arguments) == _TRUE)
+	comd = arguments[0];
+	info->command = comd;
+	if (check_builtin(info, arguments) == _TRUE)
 	return (0);
 
-status = is_field(comd);
-if (status == NON_PERMISSIONS)
+	status = is_field(comd);
+	if (status == NON_PERMISSIONS)
 {
 	info->status_code = 126;
 	info->error_code = _CODE_ACESS;
 	error(info);
 	return (0);
 }
-if (status == 1)
+	if (status == 1)
 {
 	execute(comd, arguments, buff, info);
 	return (0);
 }
 
-if (current_directory(comd, arguments, buff, info) == _TRUE)
+	if (current_dir(comd, arguments, buff, info) == _TRUE)
 	return (0);
 
 	info->value_path = which(comd, info);
@@ -42,4 +43,8 @@ if (current_directory(comd, arguments, buff, info) == _TRUE)
 	execute(info->value_path, arguments, info, buff);
 	free_memory_p((void *) info->value_path);
 	return (0);
+}
+	info->status_code = 127;
+	info->error_code = _CODE_COMD_DOES_NOT_EXIST;
+	error(info);
 }
