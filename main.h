e5bc5cf5d57859905c2dc422def08c26f0366c91
Iseyemi Oluwafemi <iseyemiabrahamf@gmail.com.com>
2023-04-27 07:55:55 -0700

    my commit

--- main.h
+++ main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib h>
+#include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
-#include <string h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
